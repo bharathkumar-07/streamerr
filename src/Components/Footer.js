5585f6261992a79948f31e7735b3bd3b0326199e
bharath <barathk777@gmail.com>
2024-09-10 20:23:31 +0530

    update error

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,18 +7,17 @@ export const Footer = () => {
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023<Link href="/" className="hover:underline">streamerr</Link>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
-            <a href="#" target='_blank' className="hover:underline me-4 md:me-6">About</a>
-        </li>
-        <li>
-            <a href="#" target='_blank' className="hover:underline me-4 md:me-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" target='_blank' className="hover:underline me-4 md:me-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" target='_blank' className="hover:underline">Contact</a>
-        </li>
+       
+            <Link target='_blank' className="hover:underline me-4 md:me-6">About</Link>
+    
+        
+            <Link target='_blank' className="hover:underline me-4 md:me-6">Privacy Policy</Link>
+ 
+       
+            <Link target='_blank' className="hover:underline me-4 md:me-6">Licensing</Link>
+      
+            <Link target='_blank' className="hover:underline">Contact</Link>
+        
     </ul>
     </div>
 </footer>
